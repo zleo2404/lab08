0594ea6651676e7f4c4532ae389400316d2bbb2a
DanySK <danilo.pianini@gmail.com>
2024-06-18 17:25:51 +0000

    Deploying to exercises from @ APICe-at-DISI/exercises@6c1aef79a75fd95704a0984e6b36e0d6af8ff5f7 🚀

--- 81-junit/src/main/java/it/unibo/bank/api/AccountHolder.java
+++ 81-junit/src/main/java/it/unibo/bank/api/AccountHolder.java
@@ -21,6 +21,8 @@ public final class AccountHolder {
     }
 
     /**
+     * The name of the account holder.
+     *
      * @return the name of the bank account holder.
      */
     public String getName() {
@@ -28,6 +30,8 @@ public final class AccountHolder {
     }
 
     /**
+     * The surname of the account holder.
+     *
      * @return the surname of the bank account holder.
      */
     public String getSurname() {
@@ -35,6 +39,8 @@ public final class AccountHolder {
     }
 
     /**
+     * The user id of the account holder.
+     *
      * @return the user id of the bank account holder.
      */
     public int getUserID() {
