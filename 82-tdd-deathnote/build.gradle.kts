328bdb1c2dc77e729c78fb128f41e1d1b67d7469
DanySK <danilo.pianini@gmail.com>
2024-11-11 12:14:06 +0000

    Deploying to exercises from @ APICe-at-DISI/exercises@9faadea9412f84273150a943d4ab5aa17263159e 🚀

--- 82-tdd-deathnote/build.gradle.kts
+++ 82-tdd-deathnote/build.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
     java
-    id("org.danilopianini.gradle-java-qa") version "1.74.0"
+    id("org.danilopianini.gradle-java-qa") version "1.75.0"
 }
 
 repositories {
